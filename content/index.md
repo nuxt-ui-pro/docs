d306b0ef607ca0086a1c6973873ba3cc6eb87229
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2023-10-12 13:08:08 +0200

    feat: add hero on homepage

--- content/index.md
+++ content/index.md
@@ -1,6 +1,19 @@
 ---
 title: 'Introduction'
 description: 'Welcome to Nuxt UI Pro documentation template.'
+hero:
+  title: '<span class="text-primary">Nuxt UI Pro</span><br>Docs template'
+  description: 'Create your documentation with our template!'
+  links:
+    - label: 'Use this template'
+      icon: 'i-simple-icons-github'
+      to: 'https://github.com/nuxt-ui-pro/docs'
+      target: '_blank'
+    - label: 'UI Pro docs'
+      icon: 'i-simple-icons-nuxtdotjs'
+      color: 'white'
+      to: 'https://ui.nuxt.com/pro'
+      target: '_blank'
 ---
 
 ## Features
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,22 @@
 <script setup>
+const route = useRoute()
+
 const { data: navigation } = useNuxtData('navigation')
+
+const { data: page, refresh: refreshPage } = await useAsyncData(`docs-${route.path}`, () => queryContent(route.path).findOne())
+
+watch(() => route.path, refreshPage)
 </script>
 
 <template>
   <UMain>
     <UContainer>
+      <ULandingHero v-if="page?.hero" v-bind="page.hero">
+        <template #title>
+          <span v-html="page.hero.title" />
+        </template>
+      </ULandingHero>
+
       <UPage>
         <template #left>
           <UAside>
