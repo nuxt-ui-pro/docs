ad896a24a519e1188830254a3b87b8db6a7f6f73
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2023-10-12 10:05:57 +0200

    chore: add social links

--- app.vue
+++ app.vue
@@ -7,16 +7,67 @@ const { data: files } = useLazyFetch('/api/search.json', {
 </script>
 
 <template>
-  <UHeader title="My Docs">
+  <UHeader>
     <template #right>
-      <UButton to="https://ui.nuxt.com/pro" icon="i-heroicons-book-open" target="_blank" color="gray" variant="ghost" />
+      <UButton
+        aria-label="Nuxt UI Pro"
+        to="https://ui.nuxt.com/pro"
+        icon="i-simple-icons-nuxtdotjs"
+        target="_blank"
+        color="gray"
+        variant="ghost"
+      />
+      <UButton
+        aria-label="Nuxt UI Pro Docs on GitHub"
+        to="https://github.com/nuxt-ui-pro/docs"
+        icon="i-simple-icons-github"
+        target="_blank"
+        color="gray"
+        variant="ghost"
+      />
+      <UButton
+        aria-label="Nuxt on X"
+        to="https://x.com/nuxt_js"
+        icon="i-simple-icons-x"
+        target="_blank"
+        color="gray"
+        variant="ghost"
+      />
       <UColorModeButton />
     </template>
   </UHeader>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-  <UFooter />
+  <UFooter>
+    <template #right>
+      <UButton
+        aria-label="Nuxt UI Pro"
+        to="https://ui.nuxt.com/pro"
+        icon="i-simple-icons-nuxtdotjs"
+        target="_blank"
+        color="gray"
+        variant="ghost"
+      />
+      <UButton
+        aria-label="Nuxt UI Pro Docs on GitHub"
+        to="https://github.com/nuxt-ui-pro/docs"
+        icon="i-simple-icons-github"
+        target="_blank"
+        color="gray"
+        variant="ghost"
+      />
+      <UButton
+        aria-label="Nuxt on X"
+        to="https://x.com/nuxt_js"
+        icon="i-simple-icons-x"
+        target="_blank"
+        color="gray"
+        variant="ghost"
+      />
+      <UColorModeButton />
+    </template>
+  </UFooter>
   <ClientOnly>
     <LazyUDocsSearch ref="search" :files="files" :navigation="navigation" />
   </ClientOnly>
