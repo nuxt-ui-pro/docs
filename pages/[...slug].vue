282e7dd2fd9508298b149dd58e03b51c611ca429
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2023-10-16 13:19:16 +0200

    fix: generated pages

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -4,7 +4,7 @@ import { withoutTrailingSlash } from 'ufo'
 
 const route = useRoute()
 
-const { data: page } = await useAsyncData(() => queryContent(route.path).findOne())
+const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
 if (!page.value) {
   throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
 }
