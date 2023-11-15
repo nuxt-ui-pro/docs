a0f6f7bca08ebacd6261f3815f98fb2d0184fcdc
Sébastien Chopin <seb@nuxt.com>
2023-11-15 15:19:54 +0100

    docs: use env instead of nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,11 +29,6 @@ export default defineNuxtConfig({
       ]
     }
   },
-  // Essential for OgImage on `nuxt generate`
-  // https://github.com/harlan-zw/nuxt-og-image/blob/c89fd4e29f56eeb00b12bc0d7e4bcb82ab459cae/src/module.ts#L127C16-L127C16
-  site: {
-    url: 'https://nuxt-ui-pro-template-docs.vercel.app'
-  },
   // Devtools / Typescript
   devtools: { enabled: true },
   typescript: { strict: false }
