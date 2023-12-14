dea9fbeb1f79f14b052ba63cc76dead152aa718a
Benjamin Canac <canacb1@gmail.com>
2023-12-14 15:38:24 +0100

    chore(home): improve hero

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,18 @@ defineOgImage({
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
+      <template #headline>
+        <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
+          <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
+            <span class="absolute inset-0" aria-hidden="true" />
+          </NuxtLink>
+
+          {{ page.hero.headline.label }}
+
+          <UIcon v-if="page.hero.headline.icon" :name="page.hero.headline.icon" class="ml-1 w-4 h-4 pointer-events-none" />
+        </UBadge>
+      </template>
+
       <template #title>
         <MDC :value="page.hero.title" />
       </template>
