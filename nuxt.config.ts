514bd411dd0718bed04a118219b76dadc092f2ed
Benjamin Canac <canacb1@gmail.com>
2024-12-28 17:29:03 +0100

    fix(nuxt.config): lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,18 +12,23 @@ export default defineNuxtConfig({
     'nuxt-og-image'
   ],
 
-  hooks: {
-    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
-
-      globals.forEach(c => c.global = true)
-    }
+  devtools: {
+    enabled: true
   },
 
   colorMode: {
     disableTransition: true
   },
+
+  routeRules: {
+    '/api/search.json': { prerender: true }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-07-11',
 
   nitro: {
     prerender: {
@@ -34,20 +39,17 @@ export default defineNuxtConfig({
     }
   },
 
-  routeRules: {
-    '/api/search.json': { prerender: true }
-  },
-
-  devtools: {
-    enabled: true
-  },
-
   typescript: {
     strict: false
   },
 
-  future: {
-    compatibilityVersion: 4
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
   },
 
   eslint: {
@@ -57,7 +59,5 @@ export default defineNuxtConfig({
         braceStyle: '1tbs'
       }
     }
-  },
-
-  compatibilityDate: '2024-07-11'
+  }
 })
